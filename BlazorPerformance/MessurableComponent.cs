4ac712ff649f2f19f7bc50ccd4da3f69209c6402
antek <antek.olszewski01@gmail.com>
2025-03-08 16:00:37 +0100

    Blazor benchmarks

---
+++ BlazorPerformance/MessurableComponent.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Diagnostics;
+
+namespace BlazorPerformance;
+
+public class MessurableComponent : ComponentBase
+{
+    protected int ElementsCount = 0;
+    private double RenderTime = 0.0;
+
+    protected readonly Stopwatch stopwatch = new();
+    private bool shouldRender = true;
+
+    protected override bool ShouldRender()
+    {
+        if (shouldRender)
+            stopwatch.Restart();
+        return shouldRender;
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        stopwatch.Stop();
+        shouldRender = false;
+        RenderTime = stopwatch.Elapsed.TotalMilliseconds;
+        if(ElementsCount != 0)
+            Console.WriteLine($"Elements count: {ElementsCount} Time: {RenderTime} ms");
+    }
+
+    protected void ForceRender()
+    {
+        shouldRender = true;
+        StateHasChanged();
+    }
+
+    protected void Clear()
+    {
+        shouldRender = true;
+        ElementsCount = 0;
+    }
+}
